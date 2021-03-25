5d9b822c0e904b7d47a50c1c56ca57334fb63d79
cys9072 <76780573+cys9072@users.noreply.github.com>
2021-03-25 12:33:35 +0900

    Add files via upload

---
+++ script_auto_slide.js
@@ -0,0 +1,29 @@
+    //스와이퍼 자동재생,네비게이션,페이지네이션
+    var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    });
+    //재생, 일시정지 버튼 함수 제이쿼리
+    $('.start').on('click', function () {
+      swiper.autoplay.start();
+      return false;
+    })
+
+    $('.stop').on('click', function () {
+      swiper.autoplay.stop();
+      return false;
+    })
+
+    //width값에 따른 이미지 src 변경 
